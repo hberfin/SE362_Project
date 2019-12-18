01ad8400f781ad3f0f6f4e4b4485213b185f2903
anilic <celik.anil@student.atilim.edu.tr>
2019-12-18 16:15:11 +0300

    basit_request.py changed by anilic

--- basit_request.py
+++ basit_request.py
@@ -3,4 +3,6 @@ import requests
 request = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 data = request.json()
 print('Baslik: ' + data['title'])
+print('This change is made by Anil Celik')
 #deneme değişiklik
+#cheers
